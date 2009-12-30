b2034eb874946c2361dedf08a0577bdea58e541e
Thetruecrow <Thetruecrow@ee1002a0-6a85-4771-8de5-1ab95472fd38>
2009-12-30 23:14:31 +0000

    Committing Trunk and Branch folders. Trunk is Aspire Hearthstone Revision 1746, modified to support patch 3.3, and not include some changes done by Pepsi. I have no clue if its stable or not, people will have to check. Core is property of Aspiredev.

---
+++ Trunk/src/hearthstone-world/HonorHandler.h
@@ -0,0 +1,34 @@
+/*
+ * Aspire Hearthstone
+ * Copyright (C) 2008 - 2009 AspireDev <http://www.aspiredev.org/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef HONORHANDLER_H
+#define HONORHANDLER_H
+
+class HonorHandler
+{
+public:
+	static int32 CalculateHonorPointsForKill(Player* pPlayer, Unit* pVictim);
+	static void RecalculateHonorFields(Player* pPlayer);
+	static void AddHonorPointsToPlayer(Player* pPlayer, uint32 uAmount);
+	static void OnPlayerKilledUnit(Player* pPlayer, Unit* pVictim);	
+	static int32 CalculateHonorPointsFormula(uint32 AttackerLevel,uint32 VictimLevel);
+};
+
+
+#endif
