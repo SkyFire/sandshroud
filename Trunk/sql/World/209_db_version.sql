e56e030cad340428f625ac843c5c10a916146598
Firehunter <Firehunter@ee1002a0-6a85-4771-8de5-1ab95472fd38>
2010-04-17 13:02:15 +0000

    +Added db version table Note for devs: Please check internal forum for further information.

---
+++ Trunk/sql/World/209_db_version.sql
@@ -0,0 +1,28 @@
+/*
+SQLyog Ultimate v8.4 Beta3
+MySQL - 5.0.51a-24+lenny3-log : Database - SADB_world
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `db_version` */
+
+DROP TABLE IF EXISTS `db_version`;
+
+CREATE TABLE `db_version` (
+  `info` varchar(4) collate utf8_unicode_ci NOT NULL,
+  `revision` int(4) unsigned NOT NULL default '0',
+  `updater` varchar(32) collate utf8_unicode_ci NOT NULL,
+  `patch_date` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`info`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
