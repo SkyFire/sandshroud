33ad591a4c453d692458cb17c8a669287c6a37f4
thetruecrow <thetruecrow@ee1002a0-6a85-4771-8de5-1ab95472fd38>
2010-05-01 19:34:37 +0000

    Branching core into one that has the event system in it before we remove it in trunk.

---
+++ Branch/EventSystemCore/sql/Aspire/character_updates/1693_petactionbar.sql
@@ -0,0 +1,47 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.04 
+MySQL - 6.0.7-alpha-community : Database - char
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*Table structure for table `playerpetactionbar` */
+
+DROP TABLE IF EXISTS `playerpetactionbar`;
+
+CREATE TABLE `playerpetactionbar` (
+  `ownerguid` bigint(20) NOT NULL DEFAULT '0',
+  `petnumber` int(11) NOT NULL DEFAULT '0',
+  `spellid_1` int(11) NOT NULL DEFAULT '0',
+  `spellid_2` int(11) NOT NULL DEFAULT '0',
+  `spellid_3` int(11) NOT NULL DEFAULT '0',
+  `spellid_4` int(11) NOT NULL DEFAULT '0',
+  `spellid_5` int(11) NOT NULL DEFAULT '0',
+  `spellid_6` int(11) NOT NULL DEFAULT '0',
+  `spellid_7` int(11) NOT NULL DEFAULT '0',
+  `spellid_8` int(11) NOT NULL DEFAULT '0',
+  `spellid_9` int(11) NOT NULL DEFAULT '0',
+  `spellid_10` int(11) NOT NULL DEFAULT '0',
+  `spellstate_1` int(11) NOT NULL DEFAULT '0',
+  `spellstate_2` int(11) NOT NULL DEFAULT '0',
+  `spellstate_3` int(11) NOT NULL DEFAULT '0',
+  `spellstate_4` int(11) NOT NULL DEFAULT '0',
+  `spellstate_5` int(11) NOT NULL DEFAULT '0',
+  `spellstate_6` int(11) NOT NULL DEFAULT '0',
+  `spellstate_7` int(11) NOT NULL DEFAULT '0',
+  `spellstate_8` int(11) NOT NULL DEFAULT '0',
+  `spellstate_9` int(11) NOT NULL DEFAULT '0',
+  `spellstate_10` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ownerguid`,`petnumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+
+ALTER TABLE `playerpets` DROP COLUMN `actionbar`;
---
+++ Branch/EventSystemCore/sql/Aspire/logon_updates/1009_accounts.sql
@@ -0,0 +1,23 @@
+-- this field is needed for ingame account ban.
+ALTER TABLE accounts MODIFY COLUMN banned int(11) NOT NULL default '0';
+ALTER TABLE `accounts` ADD `banreason` VARCHAR(512) AFTER `banned`;
+
+/* full table
+CREATE TABLE `accounts` (
+	`acct` int(5) NOT NULL AUTO_INCREMENT,
+	`login` varchar(32) NOT NULL DEFAULT '',
+	`password` varchar(255) NOT NULL DEFAULT '',
+	`SessionKey` varchar(255) NOT NULL DEFAULT '',
+	`gm` varchar(15) NOT NULL DEFAULT '0',
+	`flags` int(11) NOT NULL DEFAULT '24',
+	`banned` int(11) NOT NULL DEFAULT '0',
+	`banreason` varchar(512) DEFAULT NULL,
+	`lastlogin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`lastip` varchar(15) NOT NULL DEFAULT '',
+	`forceLanguage` varchar(5) NOT NULL DEFAULT 'enUS',
+	`email` varchar(32) DEFAULT NULL,
+	`muted` int(30) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`acct`),
+	UNIQUE KEY `login` (`login`)
+) ENGINE=MyISAM AUTO_INCREMENT=4233 DEFAULT CHARSET=utf8
+*/
