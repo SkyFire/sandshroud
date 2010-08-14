4c1a6bd3d29e18e82e9678ffaf9ab9a1a00a08e2
thetruecrow <thetruecrow@ee1002a0-6a85-4771-8de5-1ab95472fd38>
2010-08-14 08:43:03 +0000

    Oops, I deleted the event system crap... Oh well.

--- Branch/EventSystemCore/sql/Character/130_Raid_Diff.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `characters` ADD `raiddifficulty` int(10) NOT NULL DEFAULT '0' after `difficulty`;
-ALTER TABLE `groups` ADD `raiddifficulty` int(10) NOT NULL DEFAULT '0' after `difficulty`;
-ALTER TABLE `achievements` ADD `groupid` int(11) NOT NULL DEFAULT '0' after `completed`;
